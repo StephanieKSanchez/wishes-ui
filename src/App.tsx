189c7d30a80064c9b6ff8963e723fd5734d346bc
Stephanie Sanchez <stephk006@gmail.com>
2022-05-31 11:32:17 -0400

    cleaned up app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createTheme, CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/system";
+import { FC } from "react";
 
-function App() {
+const theme = createTheme({
+  typography: {
+    fontFamily: `'Noto Sans', sans-serif`,
+    fontWeightRegular: 600,
+  },
+  palette: {
+    mode: "dark",
+    background: {
+      default: "#131924",
+    },
+    primary: {
+      main: "#4d88ff",
+    },
+    secondary: {
+      main: "#a9a9a9",
+    },
+  },
+});
+
+export const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+    </ThemeProvider>
   );
-}
+};
 
-export default App;
+//FC - typescript explicitly identifying it as a functional component
